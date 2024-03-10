5fad267989af52454fc6d6c0c2a456ac1ca8bc95
Ansiya4 <125725976+Ansiya4@users.noreply.github.com>
2024-03-11 02:17:12 +0530

    Update asgi.py

--- shesquad/asgi.py
+++ shesquad/asgi.py
@@ -14,7 +14,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shesquad.settings')
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 
